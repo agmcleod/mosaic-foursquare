0a2a378b531d273c211db131fd792bcdd1679412
Joe Pym <joe@joepym.com>
2013-06-18 09:59:28 -0400

    Added author information to a photo
    This will now expose photo.user as the author, and load their profile
    picture. In cases where the user is not defined, or unknown, this will
    be nil.

--- lib/mosaic/foursquare/photo.rb
+++ lib/mosaic/foursquare/photo.rb
@@ -3,7 +3,12 @@ require 'mosaic/foursquare/object'
 module Mosaic
   module Foursquare
     class Photo < Mosaic::Foursquare::Object
-      attr_accessor :id, :url, :visibility
+      attr_accessor :id, :url, :visibility, :user
+
+      def initialize(options = {})
+        super
+        self.user &&= Mosaic::Foursquare::User.new(user)
+      end
 
       def url(size = "original")
         @url ||= @prefix.to_s + size + @suffix.to_s
--- lib/mosaic/foursquare/version.rb
+++ lib/mosaic/foursquare/version.rb
@@ -1,5 +1,5 @@
 module Mosaic
   module Foursquare
-    VERSION = "0.2.1"
+    VERSION = "0.2.2"
   end
 end
