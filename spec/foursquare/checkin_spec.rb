71d6c435c390ff6d94f9875dd45bbba79e279bb8
Billy Monk <billy.monk@mosaic.com>
2013-06-24 11:03:13 -0400

    Add tests for Mosaic::Foursquare::Checkin.find. Close #7.

--- spec/foursquare/checkin_spec.rb
+++ spec/foursquare/checkin_spec.rb
@@ -6,6 +6,21 @@ describe Mosaic::Foursquare::Checkin, :vcr => {:cassette_name => 'Venue/shared'}
       Mosaic::Foursquare.configure(:oauth_token => RSpec.configuration.oauth_token)
     end
 
+    context "and I find a checkin" do
+      let(:checkin) { Mosaic::Foursquare::Checkin.find(RSpec.configuration.checkin_id, { :signature => RSpec.configuration.checkin_signature  }) }
+      it "should create a new checkin" do
+        expect(checkin).to be_an_instance_of(Mosaic::Foursquare::Checkin)
+      end
+
+      it "should create a new user based on checkin's author" do
+        expect(checkin.user).to be_an_instance_of(Mosaic::Foursquare::User)
+      end
+
+      it "should create a new venue based on the checkin's venue" do
+        expect(checkin.venue).to be_an_instance_of(Mosaic::Foursquare::Venue)
+      end
+    end
+
     context "and I load a group of checkins from a venue" do
       let(:checkins){Mosaic::Foursquare::Venue.find(RSpec.configuration.venue_id).herenow}
 
